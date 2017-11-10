2ec3784cd36074dc92037c6c2e66e833a0a67e78
Ganesh Prabu <gprabu@yahoo.com>
2017-11-09 23:35:18 -0600

    Initial Trial Project

--- Swift_App/ViewController.swift
+++ Swift_App/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count = 0
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.view.backgroundColor = UIColor.redColor()
+        self.label.text = "This is awesome"
     }
 
+    @IBOutlet weak var button: UIButton!
+    
+    
+    @IBAction func buttonpresses(sender: AnyObject) {
+        self.label.text = "This is Button"
+        print(" Button Pressed %d times", count);
+        
+        if (count==5)
+        {
+            print( "Button Pressed 5 times");
+            count=0;
+        }
+        count++
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
