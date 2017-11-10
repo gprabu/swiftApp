52e763d0b57ace0dabdcc872c769191caa895648
Ganesh Prabu <gprabu@yahoo.com>
2017-11-09 23:36:18 -0600

    no message

--- Swift_App/ViewController.swift
+++ Swift_App/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //self.view.backgroundColor = UIColor.redColor()
+        
         self.label.text = "This is awesome"
     }
 
